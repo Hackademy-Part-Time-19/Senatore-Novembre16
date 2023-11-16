1345026d49e2c249baa0cd194ec52df93688e6d2
MattiaSen <147874985+MattiaSen@users.noreply.github.com>
2023-11-16 19:14:07 +0100

    nomi

---
+++ java.js
@@ -0,0 +1,7 @@
+let array = ["franco", "Gennaro", "Valerio", "Giulia", "eugenio"]
+        for(let i=0; i < array.length ; i++){
+            
+            console.log(array[i])
+        }
+
+        
